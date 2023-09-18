8be89739685b03a67cda4e0962ddb2ca3fffc96b
kaer98 <56426532+kaer98@users.noreply.github.com>
2023-09-18 10:33:35 +0200

    make item class

---
+++ SocketOpgave/SocketOpgave/Models/SaleItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketOpgave.Models
+{
+    public class SaleItem
+    {
+        public int Id { get; set; }
+        public float MinPrice { get; set; }
+        public float SalePrice { get; set; }
+        public TimeSpan AuctionLength { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get{ return StartTime.Add(AuctionLength); } }
+
+
+
+    }
+}
